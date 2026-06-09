2186ced5eb04b8c28142ddb8047ec42c8f3e2449
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2026-06-08 22:25:59 -0600

    feat:se agrega la capa de request y responseWrapper para capturar y generar respuestas.

---
+++ Cavex.Principal.Common/Transfer/RequestWrapper.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cavex.Principal.Common.Transfer
+{
+    public class RequestWrapper<T>
+    {
+        [Required]
+        public T Body { get; set; }
+    }
+}
---
+++ Cavex.Principal.Common/Transfer/ResponseWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Cavex.Principal.Common.Transfer
+{
+    public class ResponseWrapper<T>
+    {
+        public HttpStatusCode StatusCode { get; set; }
+
+        public string? Message { get; set; }
+
+        public T? Data { get; set; }
+    }
+}
